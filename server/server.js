1663857c5ce3c7d9e1acd87b0cca6e1ab20bde68
Anoushka <koleyanoushka27@gmail.com>
2026-09-02 22:18:54 +0530

    Fix CORS preflight

--- server/server.js
+++ server/server.js
@@ -13,17 +13,30 @@ const app = express();
 
 connectDB();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  })
-);
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173"
+  );
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS"
+  );
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 // app.options("/api/auth/login", cors());
 
