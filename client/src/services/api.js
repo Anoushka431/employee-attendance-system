27686ce24463d1ce29a4a3b953516de321fcb788
Anoushka <koleyanoushka27@gmail.com>
2026-09-02 21:55:50 +0530

    Configure frontend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api"
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  if (token) {
+  // Login request me old token nahi bhejna
+  if (token && !config.url.includes("/auth/login")) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
